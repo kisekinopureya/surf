7a788a489c42a197064485644d0b748eb3961599
kisekinopureya <gokhanozdemir753@gmail.com>
2021-05-08 03:49:16 +0300

    Applied homepage patch

--- surf.c
+++ surf.c
@@ -2153,7 +2153,11 @@ main(int argc, char *argv[])
 	if (argc > 0)
 		arg.v = argv[0];
 	else
-		arg.v = "about:blank";
+#ifdef HOMEPAGE
+		arg.v = HOMEPAGE;
+#else
+		arg.v = "https://searx.bar";
+#endif
 
 	setup();
 	c = newclient(NULL);
@@ -2167,3 +2171,4 @@ main(int argc, char *argv[])
 
 	return 0;
 }
+
